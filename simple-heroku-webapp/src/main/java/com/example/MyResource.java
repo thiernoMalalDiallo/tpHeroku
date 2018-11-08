07f3dbf758b2b613c701556f5b83acce1712239f
compte1995 <thmalalsyaka@gmail.com>
2018-11-08 11:07:01 +0100

    bien programme

--- simple-heroku-webapp/src/main/java/com/example/MyResource.java
+++ simple-heroku-webapp/src/main/java/com/example/MyResource.java
@@ -4,6 +4,8 @@ import javax.ws.rs.GET;
 import javax.ws.rs.Path;
 import javax.ws.rs.Produces;
 import javax.ws.rs.core.MediaType;
+import javax.ws.rs.*;
+
 
 /**
  * Root resource (exposed at "myresource" path)
